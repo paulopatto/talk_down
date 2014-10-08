79773ad98fb3a91df08131b20af404c68f7512e3
Paulo Patto <paulopatto@gmail.com>
2014-10-08 03:54:37 -0300

    [deploy] Update puma config. Seria bemmais fácil se fosse no Heroku.com ;-)

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: bundle exec puma -e $RAILS_ENV -p 5000 -d -S $HOME/talk_down.state -C config/puma.rb -b unix://$HOME/talk_down.sock --pidfile $HOME/puma.pid
+web: bundle exec puma -e $RAILS_ENV -C config/puma.rb
--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,22 @@
 threads 1, 6
 workers 2
 
+directory   "/opt/Applications/forum/current"
+bind        "/home/deploy/tmp/talk_down.sock"
+pidfile     "/home/deploy/tmp/talk_down.pid"
+state_path  "/home/deploy/tmp/talk_down.state"
+daemonize true
+stdout_redirect "/opt/Applications/forum/shared/log/stdout", "/opt/Applications/forum/shared/log/stderr", true
+port 5000
+
 on_worker_boot do
   require "active_record"
+  puts "Booting Puma Server"
+
   ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
   ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"])
+end
+
+on_restart do
+  puts "On restart..."
 end
