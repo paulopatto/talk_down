bfc4cc99f8b6b695691983a9f2223b7dec6adf6a
Paulo Patto <paulopatto@gmail.com>
2014-10-08 04:25:45 -0300

    [deploy] Fix puma config

--- config/puma.rb
+++ config/puma.rb
@@ -2,13 +2,12 @@ threads 1, 6
 workers 2
 
 directory   "/opt/Applications/forum/current"
-#bind        "tcp://0.0.0.0:5000"
 bind        "unix:///home/deploy/tmp/talk_down.sock"
 pidfile     "/home/deploy/tmp/talk_down.pid"
 state_path  "/home/deploy/tmp/talk_down.state"
 daemonize true
-stdout_redirect "/opt/Applications/forum/shared/log/stdout", "/opt/Applications/forum/shared/log/stderr", true
 port 5000
+stdout_redirect "/opt/Applications/forum/shared/log/puma.log", "/opt/Applications/forum/shared/log/puma.error.log", true
 
 on_worker_boot do
   require "active_record"
