fe6fd081461f5b1cb932a32fabd3bd4004ce8f5c
Paulo Patto <paulopatto@gmail.com>
2014-10-08 16:16:37 -0300

    [deploy] Fix puma and deploy set. In dev keep alive

--- config/puma.rb
+++ config/puma.rb
@@ -1,20 +1,29 @@
 threads 1, 6
 workers 2
+preload_app!
 
-directory   "/opt/Applications/forum/current"
-bind        "unix:///home/deploy/tmp/talk_down.sock"
-pidfile     "/home/deploy/tmp/talk_down.pid"
-state_path  "/home/deploy/tmp/talk_down.state"
+root_path = File.expand_path('../../', __FILE__)
+directory   root_path
+bind        "unix://#{root_path}/tmp/talk_down.sock"
+pidfile     "#{root_path}/tmp/talk_down.pid"
+state_path  "#{root_path}/tmp/talk_down.state"
 daemonize true
 port 5000
-stdout_redirect "/opt/Applications/forum/shared/log/puma.log", "/opt/Applications/forum/shared/log/puma.error.log", true
+stdout_redirect "#{root_path}/log/puma.log", "#{root_path}/log/puma.error.log", true
 
 on_worker_boot do
   require "active_record"
   puts "Booting Puma Server on port 5000..."
 
-  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-  ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"])
+
+  ActiveSupport.on_load(:active_record) do
+    config = ActiveRecord::Base.configurations[Rails.env] ||
+      Rails.application.config.database_configuration[Rails.env]
+    ActiveRecord::Base.establish_connection(config)
+  end
+
+  #ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+  #ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"])
 end
 
 on_restart do
