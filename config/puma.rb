b43efef79eec20bea403686cf4d87c63c15811ae
Paulo Patto <paulopatto@gmail.com>
2014-10-08 04:02:58 -0300

    [deploy] Fix puma config

--- config/puma.rb
+++ config/puma.rb
@@ -2,7 +2,8 @@ threads 1, 6
 workers 2
 
 directory   "/opt/Applications/forum/current"
-bind        "/home/deploy/tmp/talk_down.sock"
+#bind        "tcp://0.0.0.0:5000"
+bind        "unix:///home/deploy/tmp/talk_down.sock"
 pidfile     "/home/deploy/tmp/talk_down.pid"
 state_path  "/home/deploy/tmp/talk_down.state"
 daemonize true
@@ -11,7 +12,7 @@ port 5000
 
 on_worker_boot do
   require "active_record"
-  puts "Booting Puma Server"
+  puts "Booting Puma Server on port 5000..."
 
   ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
   ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"])
