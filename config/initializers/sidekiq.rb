655b4a7b0344100f9bd3b15d9e05d0ed7a2a2a08
Paulo Patto <paulopatto@paulopatto.com>
2016-09-18 16:14:13 -0300

    [development]: comments

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,9 @@
-# Sidekiq.configure_server do |config|
-#   config.redis = {
-#     url: 'redis://redis.example.com:7372/12'
-#   }
-# end
-#
-# Sidekiq.configure_client do |config|
-#   config.redis = {
-#     url: 'redis://redis.example.com:7372/12'
-#   }
-# end
+$sidekiq = YAML.load(File.read(File.join('config','sidekiq.yml')))[Rails.env.to_s]
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: $sidekiq[:service] }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: $sidekiq[:service] }
+end
