275e3f190562fd95d8267c8fa2e44da290d0fd39
Paulo Patto <paulopatto@paulopatto.com>
2016-09-18 16:19:24 -0300

    [development]: Try use hack overwrite service url

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,10 @@
 $sidekiq = YAML.load(File.read(File.join('config','sidekiq.yml')))[Rails.env.to_s]
 
+#HACK
+if ENV['RAILS_ENV'] == 'production'
+  $sidekiq[:service] = ENV['REDISTOGO_URL']
+end
+
 Sidekiq.configure_server do |config|
   config.redis = { url: $sidekiq[:service] }
 end
